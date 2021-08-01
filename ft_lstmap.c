4a21626c58876e160e43423f8084b7d41b9ddec8
Constantin Wastchenko <cwastche@mi-r1-p4.s19.be>
2021-08-01 15:19:30 +0200

    changes to lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cwastche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/17 16:37:40 by cwastche          #+#    #+#             */
-/*   Updated: 2021/08/01 13:40:01 by cwastche         ###   ########.fr       */
+/*   Updated: 2021/08/01 15:19:08 by cwastche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*cpy;
 	t_list	*tmp;
 
-	cpy = NULL;
-	while (lst && f && del)
+	if (lst && f && del)
 	{
-		tmp = ft_lstnew(f(lst->content));
-		if (!tmp)
-		{
-			ft_lstclear(&cpy, del);
-			return (0);
-		}
-		ft_lstadd_back(&cpy, tmp);
+		cpy = ft_lstnew(f(lst->content));
+		if (!cpy)
+			return (NULL);
 		lst = lst->next;
+		while (lst)
+		{
+			tmp = ft_lstnew(f(lst->content));
+			if (!tmp)
+			{
+				ft_lstclear(&cpy, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&cpy, tmp);
+			lst = lst->next;
+		}
+		return (cpy);
 	}
-	return (cpy);
+	return (NULL);
 }
