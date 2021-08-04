d8b534b3fdb7315cb54e39576f1d665a104bba06
Constantin Wastchenko <cwastche@mi-r1-p4.s19.be>
2021-08-04 10:24:36 +0200

    Removed (char *) malloc cast from strjoin, cleanup on some other fcts

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cwastche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 15:43:34 by cwastche          #+#    #+#             */
-/*   Updated: 2021/07/17 16:42:38 by cwastche         ###   ########.fr       */
+/*   Updated: 2021/08/04 10:22:25 by cwastche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,15 @@
 
 char	*ft_strdup(const char *s1)
 {
-	size_t	len;
 	char	*cpy;
 	int		i;
 
-	len = ft_strlen(s1);
-	cpy = malloc((len + 1));
-	i = 0;
-	if (cpy)
-	{
-		while (s1[i])
-		{
-			cpy[i] = s1[i];
-			i++;
-		}
-		cpy[i] = 0;
-	}
+	cpy = malloc((ft_strlen(s1) + 1));
+	i = -1;
+	if (!cpy)
+		return (NULL);
+	while (s1[++i])
+		cpy[i] = s1[i];
+	cpy[i] = 0;
 	return (cpy);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cwastche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 12:20:13 by cwastche          #+#    #+#             */
-/*   Updated: 2021/07/17 15:34:56 by cwastche         ###   ########.fr       */
+/*   Updated: 2021/08/04 10:19:30 by cwastche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	i = 0;
-	cpy = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
-	if (cpy)
-	{
-		while (*s1)
-			cpy[i++] = *s1++;
-		while (*s2)
-			cpy[i++] = *s2++;
-		cpy[i] = 0;
-	}
+	cpy = malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (!cpy)
+		return (NULL);
+	while (*s1)
+		cpy[i++] = *s1++;
+	while (*s2)
+		cpy[i++] = *s2++;
+	cpy[i] = 0;
 	return (cpy);
 }
